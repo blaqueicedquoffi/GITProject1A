00cee5cb3853aa35a42cf10855db7d2984851ae4
blaquevybes <blaquevybes@gmail.com>
2022-07-16 22:57:22 -0500

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Write your code here
         System.out.println("I'm the boss!!!");
+        System.out.println("I'm the boss-man!!!");
+        System.out.println("I'm the boss-lady!!!");
     }
 }

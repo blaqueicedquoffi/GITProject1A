7e144c1ceb4ed3a19256da28b999faa4d5010009
blaquevybes <blaquevybes@gmail.com>
2022-07-18 01:56:42 -0500

    5th commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,6 @@ public class Main {
        // System.out.println(10/0);
         System.out.println("I'm the newly amended code");
         System.out.println("I'm the original code");
+        System.out.println("I'm the original code master");
     }
 }
